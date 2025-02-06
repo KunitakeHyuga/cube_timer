589fc16be3cee0967b942522590a25c767284796
kunitakehyuga <hyuga20011212@gmail.com>
2025-02-07 05:04:24 +0900

    スクランブルと描画のエンドポイント作成

--- server/code/main.py
+++ server/code/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from typing import List  # ネストされたBodyを定義するために必要
+from generate_scramble import generate_scramble
+from generate_visual import generate_visual
 from starlette.middleware.cors import CORSMiddleware  # CORSを回避するために必要
 from db import session  # DBと接続するためのセッション
 from model import UserTable, User  # 今回使うモデルをインポート
@@ -26,3 +29,25 @@ def read_root():
 def read_users():
     users = session.query(UserTable).all()
     return users
+
+@app.get("/scramble")
+def get_scramble():
+    return generate_scramble()
+
+@app.get("/visual")
+def get_visual():
+    scramble = generate_scramble()
+    visual = generate_visual(scramble)
+    
+    # 1つの JSON に統合
+    response_data = {
+        "scramble": scramble["scramble"],
+        "visual": visual
+    }
+
+    return JSONResponse(content=response_data)
+
+@app.get("/array")
+def get_array():
+    array1 = [[1,2,3],[4,5,6],[7,8,9]]
+    return {"array" : array1}
